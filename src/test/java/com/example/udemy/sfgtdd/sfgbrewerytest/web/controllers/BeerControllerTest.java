301f287a22f73328446e01e9138e7843448574d6
SergeA <ronnieo@gmail.com>
2019-04-18 15:16:19 -0400

    Added List Example Test for BeerController

--- src/test/java/com/example/udemy/sfgtdd/sfgbrewerytest/web/controllers/BeerControllerTest.java
+++ src/test/java/com/example/udemy/sfgtdd/sfgbrewerytest/web/controllers/BeerControllerTest.java
@@ -2,20 +2,29 @@ package com.example.udemy.sfgtdd.sfgbrewerytest.web.controllers;
 
 import com.example.udemy.sfgtdd.sfgbrewerytest.services.BeerService;
 import com.example.udemy.sfgtdd.sfgbrewerytest.web.model.BeerDto;
+import com.example.udemy.sfgtdd.sfgbrewerytest.web.model.BeerPagedList;
 import com.example.udemy.sfgtdd.sfgbrewerytest.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -27,16 +36,13 @@ class BeerControllerTest {
 
     @Mock
     private BeerService beerService;
+    private MockMvc mockMvc;
 
-    private BeerController beerController;
-
-    MockMvc mockMvc;
-
-    BeerDto validBeer;
+    private BeerDto validBeer;
 
     @BeforeEach
     void setUp() {
-        beerController = new BeerController(beerService);
+        BeerController beerController = new BeerController(beerService);
         mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
 
         validBeer = BeerDto.builder().id(UUID.randomUUID())
@@ -60,5 +66,53 @@ class BeerControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is("Beer1")));
+    }
+
+    @DisplayName("List Ops - ")
+    @Nested
+    class TestListOperations {
+
+        @Captor
+        ArgumentCaptor<String> beerNameCaptor;
+
+        @Captor
+        ArgumentCaptor<BeerStyleEnum> beerStyleEnumCaptor;
+
+        @Captor
+        ArgumentCaptor<PageRequest> pageRequestCaptor;
+
+        BeerPagedList beerPagedList;
+
+        @BeforeEach
+        void setUp() {
+            List<BeerDto> beers = new ArrayList<>();
+            beers.add(validBeer);
+            beers.add(BeerDto.builder().id(UUID.randomUUID())
+                    .version(1)
+                    .beerName("Beer4")
+                    .upc(123123123122L)
+                    .beerStyle(BeerStyleEnum.PALE_ALE)
+                    .price(new BigDecimal("12.99"))
+                    .quantityOnHand(66)
+                    .createdDate(OffsetDateTime.now())
+                    .lastModifiedDate(OffsetDateTime.now())
+                    .build());
+
+            beerPagedList = new BeerPagedList(beers, PageRequest.of(1, 1), 2L);
+
+            given(beerService.listBeers(beerNameCaptor.capture(), beerStyleEnumCaptor.capture(),
+                    pageRequestCaptor.capture())).willReturn(beerPagedList);
+        }
+
+        @DisplayName("Test list beers - no parameters")
+        @Test
+        void testListBeers() throws Exception {
+            mockMvc.perform(get("/api/v1/beer")
+                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.content", hasSize(2)))
+                    .andExpect(jsonPath("$.content[0].id", is(validBeer.getId().toString())));
+        }
     }
 }
