e79e0ee504e428006798abfddcbd97e120f2e7f4
SergeA <ronnieo@gmail.com>
2019-04-18 12:41:16 -0400

    Initial Commit

---
+++ src/main/java/com/example/udemy/sfgtdd/sfgbrewerytest/SfgBreweryTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy.sfgtdd.sfgbrewerytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgBreweryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgBreweryTestApplication.class, args);
+	}
+
+}
